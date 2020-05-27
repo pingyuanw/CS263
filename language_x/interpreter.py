b69f74d44116dcc83134d7b9aae6c000f23f5af2
sarawang <369278452@qq.com>
2020-05-26 17:39:55 -0700

    optimize interpreter

--- language_x/interpreter.py
+++ language_x/interpreter.py
@@ -4,6 +4,15 @@ from AST import *
 import monotonic; 
 mtime = monotonic.time.time
 
+# from rpython.rlib import jit
+
+# def printable_loc(pc, frame):
+#     return str(pc)
+
+# driver = jit.JitDriver(greens = ['pc', 'frame'],
+#                        reds = ['stack', 'queue'],
+#                        get_printable_location=printable_loc)
+
 class SleepTask:
     def __init__(self, duration, callback):
         self.when = mtime() + duration
@@ -13,6 +22,7 @@ class SleepTask:
         return self.when < other.when
 class Frame:
     _pc = 0
+    _loop = 0
     func = None
     callback = None
     def __init__(self, func, callback):
@@ -22,6 +32,9 @@ class Frame:
         return self._pc >= len(self.func.sequence)
     def run(self, stack, queue):
         while not self.is_finished():
+            # required hint indicating this is the top of the opcode dispatch
+            # driver.jit_merge_point(pc=self._pc, frame=self, stack=stack, queue=queue)
+
             command = self.func.sequence[self._pc]
             self._pc += 1
 
@@ -43,8 +56,17 @@ class Frame:
                 stack.append(Frame(command.target.func, self))
                 return
             elif isinstance(command, ApplyFunc):
-                # f()
+                # f():5
+                # stack.append(Frame(command.func, None))
+                # self._loop += 1
+                # if self._loop < command.loop:
+                #     self._pc -= 1
+                # else:
+                #     self._loop = 0
                 stack.extend([Frame(command.func, None) for x in range(command.loop)])
+
+                # required hint indicating this is the end of a loop
+                # driver.can_enter_jit(pc=self._pc, frame=self, stack=stack, queue=queue)
                 return
             elif isinstance(command, Print):
                 # print('xxx')
@@ -72,13 +94,20 @@ def run_event_loop(main_function):
     stack.append(Frame(main_function, None))
     running_time = 0
     sleep_time = 0
+    queue_time = 0
     while stack or queue:
         while not stack:
             current_time = mtime()
             if queue[0].when <= current_time:
-                sleep_task = queue.pop(0)
+                i = 0
+                for task in queue:
+                    if task.when > current_time:
+                        break
+                    stack.append(task.callback)
+                    i += 1
+                queue = queue[i:]
                 # heapq.heappop(queue)
-                stack.append(sleep_task.callback)
+                queue_time += (mtime() - current_time)
             else:
                 # print('going to sleep for ' + str(queue[0].when - current_time))
                 time.sleep(queue[0].when - current_time)
@@ -94,6 +123,8 @@ def run_event_loop(main_function):
     print('<<<<<<<<<<Program finished, duration: ' + str(mtime() - start_time))
     print('<<<<<<<<<<Acutal running time: ' + str(running_time))
     print('<<<<<<<<<<Acutal sleep time: ' + str(sleep_time))
+    print('<<<<<<<<<<Queue time: ' + str(queue_time))
+
 
 
 
